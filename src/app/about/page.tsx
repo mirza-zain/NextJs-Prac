57125e3ec2313dcb58d9decb4cd557d6f9f4c2ca
mirza-zain <mirzazain269@gmail.com>
2024-10-20 12:28:02 +0500

    Navbar Created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex-1">
+      <h2>This is About Page</h2>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React, { useState } from 'react';
+
+const Navbar = () => {
+    const [mob, setmob] = useState(false)
+  return (
+    <>
+    <header className='h-16 p-4 flex justify-between items-center text-black'>
+        <h1 className='text-xl text-red-400 font-bold '>Gaming Arena</h1>
+        {!mob ? (
+            <button className='md:hidden text-lg' onClick={() => setmob(true)} title='open'><i className="ri-menu-line"></i></button>
+        ): (
+            <button onClick={() => setmob(false)} className='font-medium md:hidden' title='close'><i className="ri-close-line"></i></button>
+        )}
+        <div className='hidden md:flex justify-between items-center gap-5'>
+            <ul className='flex justify-between items-center gap-5'>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='/'>Home</a></li>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='/about'>About</a></li>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='#'>Services</a></li>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='#'>Work</a></li>
+            </ul>
+            <button className='px-4 py-2 bg-red-400 rounded-md border-2 border-black font-medium hover:bg-gray-600 hover:text-white ease-in-out duration-700'>Contact</button>
+        </div>
+    </header>
+    {mob && (
+        <div className='text-black absolute top-1/3 left-[38%] backdrop-blur-md bg-transparent flex flex-col justify-between items-center gap-5 md:hidden'>
+            <ul className='flex flex-col justify-between items-center gap-5'>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='/'>Home</a></li>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='/about'>About</a></li>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='#'>Services</a></li>
+                <li className='text-md font-semibold hover:text-red-300 ease-in-out duration-500'><a href='#'>Work</a></li>
+            </ul>
+            <button className='px-4 py-2 bg-red-400 rounded-md border-2 border-black font-medium hover:bg-gray-600 hover:text-white ease-in-out duration-700'>Contact</button>
+        </div>
+    )}
+    </>
+  )
+}
+
+export default Navbar
